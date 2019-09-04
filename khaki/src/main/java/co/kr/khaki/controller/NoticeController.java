82c8d9a1155b8d9fd9a80126a543e9b11e82ed95
byeongho.min <idabc1@naver.com>
2019-09-04 13:17:44 +0900

    민병호 - 오후 실습 전 commit

--- khaki/src/main/java/co/kr/khaki/controller/NoticeController.java
+++ khaki/src/main/java/co/kr/khaki/controller/NoticeController.java
@@ -139,7 +139,9 @@ public class NoticeController {
             String oldName = request.getHeader("file-name");
             // 파일 기본경로 _ 상세경로
             //String filePath = "D:/min/eGovFrameDev-3.7.0-64bit/workspace/.metadata/.plugins/org.eclipse.wst.server.core/tmp2/wtpwebapps/khaki/resources/photoUpload/";	//학원에서의 filepath
-            String filePath = "D:/eGovFrameDev/eGovFrameDev-3.7.0-64bit/workspace/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/khaki/resources/photoUpload/";	//집에서의 filepath
+            //String filePath = "D:/eGovFrameDev/eGovFrameDev-3.7.0-64bit/workspace/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/khaki/resources/photoUpload/";	//집에서의 filepath
+            String filePath = "D:/min/project3/khaki/workspace/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/khaki/resources/photoUpload/";	//종합본에서의 filepath
+            
             //서버경로로 설정
             String saveName = sb.append(new SimpleDateFormat("yyyyMMddHHmmss")
                           .format(System.currentTimeMillis()))
