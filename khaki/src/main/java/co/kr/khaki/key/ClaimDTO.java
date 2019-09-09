4649c8d8ce04951d3283a8e37f5cce88244196dc
kyungil <mos4657@gmail.com>
2019-09-09 11:09:19 +0900

    KEY, 신고 페이지 추가

---
+++ khaki/src/main/java/co/kr/khaki/key/ClaimDTO.java
@@ -0,0 +1,62 @@
+package co.kr.khaki.key;
+
+public class ClaimDTO {
+	private int claim_num;
+	private String claim_id;
+	private int claim_resNum;
+	private String claim_category;
+	private String claim_content;
+	private String claim_result;
+	private String claim_suspect;
+	private String claim_punishment;
+	public int getClaim_num() {
+		return claim_num;
+	}
+	public void setClaim_num(int claim_num) {
+		this.claim_num = claim_num;
+	}
+	public String getClaim_id() {
+		return claim_id;
+	}
+	public void setClaim_id(String claim_id) {
+		this.claim_id = claim_id;
+	}
+	public int getClaim_resNum() {
+		return claim_resNum;
+	}
+	public void setClaim_resNum(int claim_resNum) {
+		this.claim_resNum = claim_resNum;
+	}
+	public String getClaim_category() {
+		return claim_category;
+	}
+	public void setClaim_category(String claim_category) {
+		this.claim_category = claim_category;
+	}
+	public String getClaim_content() {
+		return claim_content;
+	}
+	public void setClaim_content(String claim_content) {
+		this.claim_content = claim_content;
+	}
+	public String getClaim_result() {
+		return claim_result;
+	}
+	public void setClaim_result(String claim_result) {
+		this.claim_result = claim_result;
+	}
+	public String getClaim_suspect() {
+		return claim_suspect;
+	}
+	public void setClaim_suspect(String claim_suspect) {
+		this.claim_suspect = claim_suspect;
+	}
+	public String getClaim_punishment() {
+		return claim_punishment;
+	}
+	public void setClaim_punishment(String claim_punishment) {
+		this.claim_punishment = claim_punishment;
+	}
+	
+	
+}
