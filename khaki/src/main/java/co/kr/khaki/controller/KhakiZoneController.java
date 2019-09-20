49e7c38de87d09516b7d6cb4265a67af943618e1
민병호 <@DESKTOP-CA3OI34>
2019-09-20 09:48:45 +0900

    민병호 - Khakizone DB작업 중

--- khaki/src/main/java/co/kr/khaki/controller/KhakiZoneController.java
+++ khaki/src/main/java/co/kr/khaki/controller/KhakiZoneController.java
@@ -1,14 +1,32 @@
 package co.kr.khaki.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import co.kr.khaki.zone.KhakiZoneDAO;
+import co.kr.khaki.zone.KhakiZoneDTO;
 
 @Controller
 public class KhakiZoneController {
 
+	
+	@Autowired
+	KhakiZoneDAO zonedao;
+	
 	@RequestMapping("khakizone_main.do")
 	public String khakizone() {
 		System.out.println("khakizone_main 소환!");
+		
+		return "khakizone/khakizone_main";
+	}
+	
+	@RequestMapping("khakizone_insert.do")
+	public String khakizone_insert(KhakiZoneDTO khakiZoneDTO) {
+		System.out.println("khakizone_insert!");
+		
+		System.out.println(khakiZoneDTO);
+		zonedao.insert(khakiZoneDTO);
 		
 		return "khakizone/khakizone_main";
 	}
