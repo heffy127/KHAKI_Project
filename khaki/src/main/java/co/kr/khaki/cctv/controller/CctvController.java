bb7b641a8086ad4c2901eb51fd225dba0bcfd49e
민병호 <@DESKTOP-CA3OI34>
2019-10-04 12:56:29 +0900

    민병호 - cctv 컨트롤러 분리

--- khaki/src/main/java/co/kr/khaki/cctv/controller/CctvController.java
+++ khaki/src/main/java/co/kr/khaki/cctv/controller/CctvController.java
@@ -1,4 +1,4 @@
-package co.kr.khaki.controller;
+package co.kr.khaki.cctv.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
